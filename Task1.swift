4b14492083abd455bd7868a6c32b6ee87b06b9ed
v-grigoreva1302 <162181989+v-grigoreva1302@users.noreply.github.com>
2024-03-27 00:37:08 +0300

    Create Task1.swift

---
+++ Task1.swift
@@ -0,0 +1,14 @@
+// Enum с RawValue типом Int
+enum Week: Int {
+    case sunday = 1
+    case monday = 2
+    case tuesday = 3
+}
+
+// Enum с RawValue типом String
+enum Direction: String {
+    case north = "North"
+    case south = "South"
+    case east = "East"
+    case west = "West"
+}
