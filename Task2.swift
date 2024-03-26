7ec7c530fad4f3b7d0b5bf0e77251ec91828cf0d
v-grigoreva1302 <162181989+v-grigoreva1302@users.noreply.github.com>
2024-03-27 00:38:53 +0300

    Create Task2.swift

---
+++ Task2.swift
@@ -0,0 +1,16 @@
+enum Gender: String {
+    case male = "Male"
+    case female = "Female"
+}
+
+enum AgeCategory: Int {
+    case child = 0
+    case teenager = 1
+    case adult = 2
+}
+
+enum Experience: String {
+    case junior = "Junior"
+    case middle = "Middle"
+    case senior = "Senior"
+}
